6d39e29669ca8f1ff1c6cb072b76fe26cde80b05
gebeto <slavik.nychkalo@gmail.com>
2023-11-16 12:37:39 +0200

    remove duplicate code

--- examples/storybook-webpack/.storybook/main.ts
+++ examples/storybook-webpack/.storybook/main.ts
@@ -7,7 +7,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "storybook-addon-manual-mocks",
+    "storybook-addon-manual-mocks/webpack",
   ],
   framework: {
     name: "@storybook/react-webpack5",
