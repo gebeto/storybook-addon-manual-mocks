00c4635f4e0139b3bfae426484e04c6c63a33e87
gebeto <slavik.nychkalo@gmail.com>
2023-11-16 12:28:48 +0200

    add correct webpack example

---
+++ examples/storybook-webpack/webpack/webpack.dev.js
@@ -0,0 +1,18 @@
+const webpack = require("webpack");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "cheap-module-source-map",
+  devServer: {
+    hot: true,
+    open: true,
+    port: 1111,
+  },
+  plugins: [
+    new ReactRefreshWebpackPlugin(),
+    new webpack.DefinePlugin({
+      "process.env.name": JSON.stringify("Vishwas"),
+    }),
+  ],
+};
