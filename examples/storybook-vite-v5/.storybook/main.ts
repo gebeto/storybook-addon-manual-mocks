4c7e17aa608ce4f7a496ae458991698e827b6716
Yaroslav Nychkalo <slavik.nychkalo@gmail.com>
2024-02-16 14:55:30 +0200

    Allow to use this package with any version of vite or webpack (#16)

---
+++ examples/storybook-vite-v5/.storybook/main.ts
@@ -0,0 +1,25 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../**/*.mdx", "../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+    {
+      name: "storybook-addon-manual-mocks/vite",
+      options: {
+        mocksFolder: "__mocks__",
+      },
+    },
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
