f518f65c910931e280329491864c6eec254a8454
gebeto <slavik.nychkalo@gmail.com>
2025-07-01 09:58:32 +0300

    add new approach for mocks for vite

--- vite/preset.js
+++ vite/preset.js
@@ -1,16 +1,16 @@
 const path = require("path");
 const fs = require("fs");
 
-const createViteManualMocksPlugin = (mocksDirectoryPath) => {
+const createViteManualMocksDirectoryPlugin = (mocksFolderPath) => {
   return function parcelManualMocksPlugin() {
     return {
-      name: "manual-mocks-plugin",
+      name: "manual-mocks-directory-plugin",
       load(_importPath) {
         const importPath = _importPath.replace(/\0/g, "");
         const basePath = path.parse(importPath);
         const mockPath = path.join(
           basePath.dir,
-          mocksDirectoryPath,
+          mocksFolderPath,
           basePath.base
         );
         const isReplacementPathExists = fs.existsSync(mockPath);
@@ -22,12 +22,46 @@ const createViteManualMocksPlugin = (mocksDirectoryPath) => {
   };
 };
 
+const createViteManualMocksPostfixPlugin = () => {
+  function parcelMocksPlugin() {
+    return {
+      name: "manual-mocks-postfix-plugin",
+      load(_importPath) {
+        const importPath = _importPath.replace(/\0/g, "");
+        const basePath = path.parse(importPath);
+        const mockPath = path.join(
+          basePath.dir,
+          basePath.base
+            .replace(/\.js$/, ".mock.js")
+            .replace(/\.ts$/, ".mock.ts")
+            .replace(/\.jsx$/, ".mock.jsx")
+            .replace(/\.tsx$/, ".mock.tsx")
+        );
+
+        const isReplacementPathExists = fs.existsSync(mockPath);
+        if (isReplacementPathExists) {
+          return fs.readFileSync(mockPath, { encoding: "utf8" });
+        }
+      },
+    };
+  }
+};
+
+const createViteManualMocksPlugin = (config) => {
+  if (config.approach === "directory") {
+    return createViteManualMocksDirectoryPlugin(config.mocksFolder);
+  } else if (config.approach === "postfix") {
+    return createViteManualMocksPostfixPlugin();
+  }
+};
+
 module.exports = {
   async viteFinal(config, context) {
     const { mergeConfig } = await import("vite");
-    const parcelManualMocksPlugin = createViteManualMocksPlugin(
-      context.mocksFolder || "__mocks__"
-    );
+    const parcelManualMocksPlugin = createViteManualMocksPlugin({
+      approach: context.approach || "directory",
+      mocksFolder: context.mocksFolder || "__mocks__",
+    });
 
     return mergeConfig(config, {
       plugins: [parcelManualMocksPlugin()],
